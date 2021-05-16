2c51806a6914216307524a852adcb6a40d96d7ac
Kirill Romanenko <brs.group.com@gmail.com>
2021-05-16 23:20:18 +0300

    task 23

--- src/module_2.js
+++ src/module_2.js
@@ -14,10 +14,10 @@ function checkPassword(password) {
   const ADMIN_PASSWORD = "jqueryismyjam";
   // Change code below this line
 
-  if (password === ADMIN_PASSWORD){
-  	return "Welcome!";
+  if (password === ADMIN_PASSWORD) {
+    return "Welcome!";
   }
-  	return "Access denied, wrong password!";
+  return "Access denied, wrong password!";
   // Change code above this line
 }
 
@@ -25,11 +25,13 @@ function checkPassword(password) {
 
 function checkStorage(available, ordered) {
   // Change code below this line
-  
+
   if (ordered === 0) {
-    return "Your order is empty!";}
+    return "Your order is empty!";
+  }
   if (ordered > available) {
-    return "Your order is too large, not enough goods in stock!";} 
+    return "Your order is too large, not enough goods in stock!";
+  }
   return "The order is accepted, our manager will contact you";
 
   // Change code above this line
@@ -60,9 +62,9 @@ const lastElement = fruits[lastElementIndex];
 // array extreme points method (task 9)
 
 function getExtremeElements(array) {
-    
+
   const firstElement = array[0];
-  
+
   const lastElementIndex = array.length - 1;
   const lastElement = array[lastElementIndex];
 
@@ -74,7 +76,7 @@ function getExtremeElements(array) {
 function splitMessage(message, delimeter) {
   let words;
 
-	words = message.split(delimeter);
+  words = message.split(delimeter);
 
   return words;
 }
@@ -83,11 +85,11 @@ function splitMessage(message, delimeter) {
 
 function calculateEngravingPrice(message, pricePerWord) {
 
-let wordCounter;
-  
+  let wordCounter;
+
   wordCounter = message.split(" ");
-  
-const totalPrice = (wordCounter.length * pricePerWord);
+
+  const totalPrice = (wordCounter.length * pricePerWord);
 
   return totalPrice;
 }
@@ -97,7 +99,7 @@ const totalPrice = (wordCounter.length * pricePerWord);
 function makeStringFromArray(array, delimeter) {
   let string;
 
-string = array.join(delimeter);
+  string = array.join(delimeter);
 
   return string;
 }
@@ -105,23 +107,23 @@ string = array.join(delimeter);
 // slug generator method (task 13)
 
 function slugify(title) {
-    const normalizedInput = title.toLowerCase();
+  const normalizedInput = title.toLowerCase();
 
-    let words;
+  let words;
 
-	words = normalizedInput.split(' ');
+  words = normalizedInput.split(' ');
 
-    const slug = words.join("-");
+  const slug = words.join("-");
 
-    return slug;
+  return slug;
 }
 
 // array slice() method (task 14)
 
 const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
 
-const firstTwoEls = fruits.slice(0,2);
-const nonExtremeEls = fruits.slice(1,4);
+const firstTwoEls = fruits.slice(0, 2);
+const nonExtremeEls = fruits.slice(1, 4);
 const lastThreeEls = fruits.slice(2, 5);
 
 // concat() method (task 15)
@@ -138,7 +140,7 @@ function makeArray(firstArray, secondArray, maxLength) {
 
   const array = firstArray.concat(secondArray);
   const newArray = array.slice(0, maxLength);
-	return newArray;
+  return newArray;
 }
 
 // for method (task 17)
@@ -146,26 +148,79 @@ function makeArray(firstArray, secondArray, maxLength) {
 const start = 3;
 const end = 7;
 
-for (let i = start; i <= end; i += 1) { 
+for (let i = start; i <= end; i += 1) {
   console.log(i);
 }
 
 // for + total method (task 18)
 
 function calculateTotal(number) {
- 
-  let totalSum = 0;
+  let total = 0;
+  for (i = 1; i <= number; i += 1) {
+    total += i;
+  }
+  return total;
+
+}
+
+// for + total method (task 19)
+
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+for (let i = 0; i < fruits.length; i += 1) {
+  const fruit = fruits[i];
+  console.log(fruit);
+}
+
+// for + total method (task 20)
+
+function calculateTotalPrice(order) {
+  let total = 0;
+  
+  for (let i = 0; i <= order.length - 1; i += 1){
+   total += order[i];
+  }
+  
+  return total;
+}
+
+// for + string method (task 21)
+
+function findLongestWord(string) {
+
+   const words = string.split(' ');
+  
+   let longestWord = words [0];
+  
+  for ( const word of words) {
+  	if (word.length > longestWord.length){
+    longestWord = word;
+    }
+  }
+  return longestWord;
+}
+
+// .push() method (task 22)
+
+function createArrayOfNumbers(min, max) {
   const numbers = [];
-
-for (let i = 1; i <= number.length; i += 1) {
-  numbers.push(`${i}`);
+  
+  for (let i = min; i <= max; i += 1){
+  	numbers.push(i); 
+  }
+  
+  return numbers;
 }
 
-console.log('numbers: ', numbers);
- 
-  totalSum += numbers;
+// filter array method (task 23)
 
- return totalSum;
+function filterArray(numbers, value) {
+const newArray = [];
+  
+  for (let i = 0; i < numbers.length; i += 1){
+  	newArray.push(i > value); 
+  }
+  
+  return newArray; 
 }
-
 
